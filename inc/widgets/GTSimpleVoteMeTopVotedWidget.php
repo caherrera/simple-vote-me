c8f4df924c870d6adf4e8d0877e7a7a404d9775b
Carlos Herrera <carlos.herrera@itlinesoftware.com>
2017-12-28 12:18:06 -0300

    upgrade to wordpress 4.8.3

--- inc/widgets/GTSimpleVoteMeTopVotedWidget.php
+++ inc/widgets/GTSimpleVoteMeTopVotedWidget.php
@@ -73,9 +73,26 @@ class GTSimpleVoteMeTopVotedWidget extends GTSimpleVoteMeBaseWidget
         }
 
         echo '<ul>';
-        echo gt_simplevoteme_get_highest_voted_posts($instance['numberofposts']);
+        echo $this->gt_simplevoteme_get_highest_voted_posts($instance['numberofposts']);
         echo '</ul>';
         echo $after_widget;
+    }
+
+    //Order by total votes
+    function gt_simplevoteme_get_highest_voted_posts($numberofpost)
+    {
+        $output    = '';
+        $the_query = new WP_Query('meta_key=_simplevotemetotal&amp;orderby=meta_value_num&amp;order=DESC&amp;posts_per_page=' . $numberofpost);
+        // The Loop
+        while ($the_query->have_posts()) : $the_query->the_post();
+            $output .= '<li>';
+            $output .= '<a href="' . get_permalink() . '" rel="bookmark">' . get_the_title() . '(' . get_post_meta(get_the_ID(),
+                    '_simplevotemetotal', true) . ')' . '</a> ';
+            $output .= '</li>';
+        endwhile;
+        wp_reset_postdata();
+
+        return $output;
     }
 
 }
